64b533c3fa4d3809423fa1efe44f2aca44e41e89
zoon <zoon@xman.com>
2017-09-25 23:17:33 +0700

    添加regist

--- main.go
+++ main.go
@@ -4,34 +4,63 @@ import (
 	"net/http"
 	"io"
 	"fmt"
+	"strings"
 )
 
 func hello(rw http.ResponseWriter, rq *http.Request) {
 	io.WriteString(rw, "hello world")
 }
 
+//define http method
+const (
+	GET, POST, PUT, DELETE = "GET", "POST", "PUT", "DELETE"
+)
+
+var METHOD_MAP = []string{GET, POST, PUT, DELETE}
+
 // handle
 type moonHandle struct {
 	routerMap map[string]interface{} //path map   end with requestHandle
-
 }
 
-func (moonHandle) RegisterRouter() {
+func (*moonHandle) RegisterRouter(methods []string, path string, controller func(rw http.ResponseWriter, pr *http.Request)) {
+	for _, v := range methods {
+		for _,vMehtod := range METHOD_MAP{
+			if strings.ToUpper(v) == vMehtod{
 
+			}
+		}
+	}
 }
 
+func routePath(rw http.ResponseWriter, pr *http.Request) {
+	io.WriteString(rw, "xman path")
+}
 
+func urlRoutePath(rw http.ResponseWriter, pr *http.Request) {
+	io.WriteString(rw, "url aaa xman path")
+}
+
+func (*moonHandle) match(realPath []string) func(rw http.ResponseWriter, pr *http.Request) {
+
+}
 
 func (moonHandle) ServeHTTP(rw http.ResponseWriter, pr *http.Request) {
-	fmt.Println(pr.RequestURI, pr.Host, pr.URL.Path , pr.Method)
+	//fmt.Println(pr.RequestURI, pr.Host, pr.URL.Path, pr.Method)
+
+	paths := strings.Split(pr.URL.Path, "/")
+	fmt.Println(paths)
+	var firstPath []string
+	firstPath[0] = "/"
+	realPath := append(firstPath, paths ...)
 
 	//router
-	if pr.URL.Path == "/"{
-
+	if pr.URL.Path == "/" {
+		routePath(rw, pr)
 	}
 
-	if pr.URL.Path == "/aaa"{
-
+	if pr.URL.Path == "/aaa" {
+		urlRoutePath(rw, pr)
 	}
 
 }
@@ -42,6 +71,9 @@ type moon struct {
 func (moon) Run(addr string) {
 	//interface struct
 	moonh := &moonHandle{}
+	moonh.RegisterRouter([]string{"GET", "POST"}, "/", routePath)
+	moonh.RegisterRouter([]string{"GET", "POST"}, "/aaaaa", urlRoutePath)
+
 	http.ListenAndServe(addr, moonh)
 }
 
